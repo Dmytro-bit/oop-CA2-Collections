dd360fcb2864f7e929d884a98d69fc55336ec878
Vlad90kg <vladbrovi@gmail.com>
2024-11-29 11:15:12 +0000

    Implemented Dijkstra algorithm

--- src/Question11.java
+++ src/Question11.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.TreeSet;
 
@@ -11,6 +12,8 @@ public class Question11
 //      Test Test Test
         String city1,city2;
         int distance;
+        String from = "";
+        int counter = 0;
         File file = new File("citiesQ11.txt");
         Scanner scanner =  new Scanner(file);
         HashMap<String, TreeSet<DistanceTo>> directRoutes = new HashMap<>();
@@ -20,7 +23,10 @@ public class Question11
             distance = scanner.nextInt();
             DistanceTo distanceTo2 = new DistanceTo(city2,distance);
             DistanceTo distanceTo1 = new DistanceTo(city1,distance);
-
+            if(counter<1){
+                from = city1;
+                counter++;
+            }
 
             directRoutes.putIfAbsent(city1, new TreeSet<>());
             directRoutes.putIfAbsent(city2, new TreeSet<>());
@@ -29,7 +35,29 @@ public class Question11
             directRoutes.get(city2).add(distanceTo1);
         }
 
-        System.out.println(directRoutes);
+        DistanceTo distance0 = new DistanceTo(from, 0);
+        PriorityQueue<DistanceTo> pq = new PriorityQueue<>();
+        HashMap<String, Integer> shortestKnownDistances = new HashMap<>();
 
+        pq.add(distance0);
+        while(!pq.isEmpty()){
+            DistanceTo smallest = pq.poll();
+            String target = smallest.getTarget();
+            if(!shortestKnownDistances.containsKey(target)){
+                int d = smallest.getDistance();
+                shortestKnownDistances.put(target,d);
+                directRoutes.forEach((key, value) ->{
+                    value.forEach(distanceTo -> {
+                        if(distanceTo.getTarget().equals(target)){
+                            pq.add(new DistanceTo(key,d+distanceTo.getDistance()));
+                        }
+                    });
+                });
+            }
+
+        }
+        shortestKnownDistances.forEach((key, value) -> {
+            System.out.println(key + " - " + value);
+        });
     }
 }
