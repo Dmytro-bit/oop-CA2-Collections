1f5542b0597f665e4648b2a379f1df6b7413ff38
dmytro <dziumanenkod@gmail.com>
2024-12-05 23:46:49 +0000

    Question3

--- src/Question3.java
+++ src/Question3.java
@@ -1,4 +1,7 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class Question3 {   //Nested HTML (Stack)
@@ -7,7 +10,28 @@ public class Question3 {   //Nested HTML (Stack)
 filename: name of the file to test.
 */
     public static boolean validate(String filename) throws FileNotFoundException {
-        return false;
+        Deque<String> stack = new ArrayDeque<>();
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split(" ");
+
+            for (String tag : line) {
+                if (tag.startsWith("</")) {
+
+                    if (stack.isEmpty()) return false;
+
+                    if (!stack.peek().equals(tag.replace("</", "<"))) {
+                        return false;
+                    }
+                    stack.pop();
+                } else {
+                    stack.push(tag);
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
     /*
