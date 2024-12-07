ffcb6e3cbdac89fb92040184d58c4e6cbf74ce06
Dmytro Bokov <dimabokov5@gmail.com>
2024-12-07 15:50:21 +0000

    Question 9 Done. Calculator cannot resolve numbers > 9 or < 0

--- src/Question9.java
+++ src/Question9.java
@@ -1,16 +1,73 @@
-
+import java.util.Stack;
 import java.util.Scanner;
-
 
 public class Question9 {
 
-    /*
-        Reads in an equation from the user
-     */
+    public static int calculate()
+    {
+        Scanner input = new Scanner(System.in);
+        Stack<Integer> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        System.out.println("Enter the expression: ");
+        String simpleOperators = "+-*/";
+        String expression = input.nextLine();
+
+        for (int i = 0; i < expression.length(); i++)
+        {
+            char currChar = expression.charAt(i);
+
+            if(currChar == '(')
+                operators.push(currChar);
+            else if (simpleOperators.indexOf(currChar) >= 0)
+            {
+                while(!operators.isEmpty() && (operators.peek() == '*' || operators.peek() == '/'))
+                    evaluateTop(operators.pop(), numbers);
+                operators.push(currChar);
+            }
+            else if (currChar == ')')
+            {
+                while(!operators.isEmpty() && operators.peek() != '(')
+                    evaluateTop(operators.pop(), numbers);
+                operators.pop();
+            }
+            else
+            {
+                numbers.push(Integer.parseInt(String.valueOf(currChar)));
+            }
+        }
+
+        while(!operators.isEmpty())
+        {
+            evaluateTop(operators.pop(), numbers);
+        }
+
+        return numbers.pop();
+
+    }
+
+    public static void evaluateTop(char operator, Stack<Integer> numbers)
+    {
+        switch (operator)
+        {
+            case '+':
+                numbers.push(numbers.pop() + numbers.pop());
+                break;
+            case '-':
+                int numToSubtract = numbers.pop();
+                numbers.push(numbers.pop() - numToSubtract);
+                break;
+            case '*':
+                numbers.push(numbers.pop() * numbers.pop());
+                break;
+            case '/':
+                int numToDivide = numbers.pop();
+                numbers.push(numbers.pop() / numToDivide);
+                break;
+        }
+    }
+
+
     public static void main(String[] args) {
-        String equation;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please enter equation");
-        equation = in.nextLine().trim();
+        System.out.println(calculate());
     }
 }
