5c8de6a54583353d36bf08e0b15bff437232405e
dmytro <dziumanenkod@gmail.com>
2024-12-11 11:31:34 +0000

    Fix Question7, Question8

--- src/Question7.java
+++ src/Question7.java
@@ -1,9 +1,11 @@
+import question7and8Extra.Block;
+
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
 
-import question7and8Extra.Block;
 // Shares Tax Calculations (Queue)
+
 /*
     Will repeatedly ask the user to enter the commands in the format
     buy qty price
@@ -12,63 +14,65 @@ import question7and8Extra.Block;
     or
     quit
      */
-public static void main(String[] args) {
+public class Question7 {
+    public static void main(String[] args) {
 
-    Scanner in = new Scanner(System.in);
-    String command;
+        Scanner in = new Scanner(System.in);
+        String command;
 //        declared Queue<Block> shares;
-    Queue<Block> shares = new ArrayDeque<>();
+        Queue<Block> shares = new ArrayDeque<>();
 
-    do {
-        System.out.print(">");
-        command = in.next();
-        if (command.equalsIgnoreCase("buy")) {
-            System.out.println("Quantity to buy:");
-            int qty = in.nextInt();
-            System.out.println("Price:");
-            double price = in.nextDouble();
-            shares.add(new Block(qty, price));
+        do {
+            System.out.print(">");
+            command = in.next();
+            if (command.equalsIgnoreCase("buy")) {
+                System.out.println("Quantity to buy:");
+                int qty = in.nextInt();
+                System.out.println("Price:");
+                double price = in.nextDouble();
+                shares.add(new Block(qty, price));
 
-        } else if (command.equals("sell")) {
-            System.out.println("Quantity to sell:");
-            int qty = in.nextInt();
-            System.out.println("Sell price:");
-            double price = in.nextDouble();
-            double profit = 0.0;
-            int sold = 0;
-            Block toSell;
-            double localProfit = 0;
-            do {
+            } else if (command.equals("sell")) {
+                System.out.println("Quantity to sell:");
+                int qty = in.nextInt();
+                System.out.println("Sell price:");
+                double price = in.nextDouble();
+                double profit = 0.0;
+                int sold = 0;
+                Block toSell;
+                double localProfit = 0;
+                do {
                     /*inside a do-wile loop program in each iteration if queue is not empty,
                      program peeks toSell Block to retrieve and modify element, but not remove it from queue*/
-                if (!shares.isEmpty()) {
-                    toSell = shares.peek();
-                    // if difference between shares to be sold and shares inside object > 0 we count the local profit and remove element
-                    if ((qty - toSell.getQuantity()) > 0) {
-                        qty -= toSell.getQuantity();
-                        sold += toSell.getQuantity();
-                        localProfit = toSell.getQuantity() * price - toSell.getCost();
+                    if (!shares.isEmpty()) {
+                        toSell = shares.peek();
+                        // if difference between shares to be sold and shares inside object > 0 we count the local profit and remove element
+                        if ((qty - toSell.getQuantity()) > 0) {
+                            qty -= toSell.getQuantity();
+                            sold += toSell.getQuantity();
+                            localProfit = toSell.getQuantity() * price - toSell.getCost();
 
-                        shares.poll();
+                            shares.poll();
+                        } else {
+                            // else local profit should be calculated as a cost of shares left to be sold  - their cost with old price
+                            localProfit = qty * price - qty * toSell.getPrice();
+                            int oldQty = toSell.getQuantity();
+                            toSell.setQuantity(toSell.getQuantity() - qty);
+                            int newQty = toSell.getQuantity();
+                            sold += oldQty - newQty;
+                            qty = 0;
+                        }
+                        profit += localProfit;
+
                     } else {
-                        // else local profit should be calculated as a cost of shares left to be sold  - their cost with old price
-                        localProfit = qty * price - qty * toSell.getPrice();
-                        int oldQty = toSell.getQuantity();
-                        toSell.setQuantity(toSell.getQuantity() - qty);
-                        int newQty = toSell.getQuantity();
-                        sold += oldQty - newQty;
-                        qty = 0;
+                        System.out.println("No stocks left");
+                        break;
                     }
-                    profit += localProfit;
 
-                } else {
-                    System.out.println("No stocks left");
-                    break;
-                }
-
-            } while (qty > 0);
-            System.out.println("Sold " + sold + " shares");
-            System.out.println("Profit " + profit);
-        }
-    } while (!command.equalsIgnoreCase("quit"));
+                } while (qty > 0);
+                System.out.println("Sold " + sold + " shares");
+                System.out.println("Profit " + profit);
+            }
+        } while (!command.equalsIgnoreCase("quit"));
+    }
 }
--- src/Question8.java
+++ src/Question8.java
@@ -12,86 +12,88 @@ import java.util.*;
    quit
     */
 
-static Scanner in = new Scanner(System.in);
+public class Question8 {
+    static Scanner in = new Scanner(System.in);
 
-        public static void main(String[] args) {
-            Map<String, Queue<Block>> map = new HashMap<>();
-            String command;
-            do {
-                System.out.print(">");
-                command = in.next();
-                if (command.equalsIgnoreCase("buy")) {
-                    String company = in.next();
-                    map.putIfAbsent(company, new ArrayDeque<>());
-                    Queue<Block> shares = buy(map.get(company));
-                    map.put(company, shares);
-                } else if (command.equals("sell")) {
-                    displayCompanies(map);
-                    System.out.println("Company: ");
-                    String company = in.next();
-                    Queue<Block> shares = map.get(company);
-                    sell(shares);
-                }
-            } while (!command.equalsIgnoreCase("quit"));
-        }
+    public static void main(String[] args) {
+        Map<String, Queue<Block>> map = new HashMap<>();
+        String command;
+        do {
+            System.out.print(">");
+            command = in.next();
+            if (command.equalsIgnoreCase("buy")) {
+                String company = in.next();
+                map.putIfAbsent(company, new ArrayDeque<>());
+                Queue<Block> shares = buy(map.get(company));
+                map.put(company, shares);
+            } else if (command.equals("sell")) {
+                displayCompanies(map);
+                System.out.println("Company: ");
+                String company = in.next();
+                Queue<Block> shares = map.get(company);
+                sell(shares);
+            }
+        } while (!command.equalsIgnoreCase("quit"));
+    }
 
-        public static Queue<Block> buy(Queue<Block> companyShares) {
-            System.out.println("Quantity to buy:");
-            int qty = in.nextInt();
-            System.out.println("Price:");
-            double price = in.nextDouble();
-            companyShares.add(new Block(qty, price));
-            return companyShares;
-        }
+    public static Queue<Block> buy(Queue<Block> companyShares) {
+        System.out.println("Quantity to buy:");
+        int qty = in.nextInt();
+        System.out.println("Price:");
+        double price = in.nextDouble();
+        companyShares.add(new Block(qty, price));
+        return companyShares;
+    }
 
-        public static void sell(Queue<Block> shares) {
-            System.out.println("Quantity to sell:");
-            int qty = in.nextInt();
-            System.out.println("Sell price:");
-            double price = in.nextDouble();
-            double profit = 0.0;
-            int sold = 0;
-            Block toSell;
-            double localProfit;
-            do {
+    public static void sell(Queue<Block> shares) {
+        System.out.println("Quantity to sell:");
+        int qty = in.nextInt();
+        System.out.println("Sell price:");
+        double price = in.nextDouble();
+        double profit = 0.0;
+        int sold = 0;
+        Block toSell;
+        double localProfit;
+        do {
                     /*inside a do-wile loop program in each iteration if queue is not empty,
                      program peeks toSell Block to retrieve and modify element, but not remove it from queue*/
-                if (!shares.isEmpty()) {
-                    toSell = shares.peek();
-                    // if difference between shares to be sold and shares inside object > 0 we count the local profit and remove element
-                    if ((qty - toSell.getQuantity()) > 0) {
-                        qty -= toSell.getQuantity();
-                        sold += toSell.getQuantity();
-                        localProfit = toSell.getQuantity() * price - toSell.getCost();
+            if (!shares.isEmpty()) {
+                toSell = shares.peek();
+                // if difference between shares to be sold and shares inside object > 0 we count the local profit and remove element
+                if ((qty - toSell.getQuantity()) > 0) {
+                    qty -= toSell.getQuantity();
+                    sold += toSell.getQuantity();
+                    localProfit = toSell.getQuantity() * price - toSell.getCost();
 
-                        shares.poll();
-                    } else {
-                        // else local profit should be calculated as a cost of shares left to be sold  - their cost with old price
-                        localProfit = qty * price - qty * toSell.getPrice();
-                        int oldQty = toSell.getQuantity();
-                        toSell.setQuantity(toSell.getQuantity() - qty);
-                        int newQty = toSell.getQuantity();
-                        sold += oldQty - newQty;
-                        qty = 0;
-                    }
-                    profit += localProfit;
-
+                    shares.poll();
                 } else {
-                    System.out.println("No stocks left");
-                    break;
+                    // else local profit should be calculated as a cost of shares left to be sold  - their cost with old price
+                    localProfit = qty * price - qty * toSell.getPrice();
+                    int oldQty = toSell.getQuantity();
+                    toSell.setQuantity(toSell.getQuantity() - qty);
+                    int newQty = toSell.getQuantity();
+                    sold += oldQty - newQty;
+                    qty = 0;
                 }
+                profit += localProfit;
 
-            } while (qty > 0);
-            System.out.println("Sold " + sold + " shares");
-            System.out.println("Profit " + profit);
-        }
+            } else {
+                System.out.println("No stocks left");
+                break;
+            }
 
-        public static void displayCompanies(Map<String, Queue<Block>> map){
-            map.forEach((company, shares)->{
-                int total = shares.stream()
-                        .mapToInt(Block::getQuantity)
-                        .sum();
+        } while (qty > 0);
+        System.out.println("Sold " + sold + " shares");
+        System.out.println("Profit " + profit);
+    }
 
-                System.out.println("Company: " + company + " Shares: " + total);
-            });
-        }
+    public static void displayCompanies(Map<String, Queue<Block>> map) {
+        map.forEach((company, shares) -> {
+            int total = shares.stream()
+                    .mapToInt(Block::getQuantity)
+                    .sum();
+
+            System.out.println("Company: " + company + " Shares: " + total);
+        });
+    }
+}
